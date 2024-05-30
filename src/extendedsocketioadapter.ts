a724c5c721e285e48f912c3f16941d8aab34049b
vigneshjp89 <vickyjaya007@gmail.com>
2024-05-30 18:08:09 +0530

    initial commit3

--- src/extendedsocketioadapter.ts
+++ src/extendedsocketioadapter.ts
@@ -8,7 +8,6 @@ export class ExtendedSocketIoAdapter extends IoAdapter {
         super();
 
         const options = {
-            path:process.env.SPATH,
             cors: {
                 origin: true,
                 methods: ["GET", "POST"],
